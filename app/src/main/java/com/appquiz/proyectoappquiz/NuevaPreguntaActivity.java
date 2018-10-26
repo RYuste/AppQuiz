99e759f7125e8c3598670a987d0d8491fb4a9ace
Rafael Yuste <teka_ryj@hotmail.com>
2018-10-26 12:52:16 +0200

    Permisos de escritura al pulsar Guardar en Nueva Pregunta

--- app/src/main/java/com/appquiz/proyectoappquiz/NuevaPreguntaActivity.java
+++ app/src/main/java/com/appquiz/proyectoappquiz/NuevaPreguntaActivity.java
@@ -1,11 +1,18 @@
 package com.appquiz.proyectoappquiz;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
@@ -19,6 +26,10 @@ public class NuevaPreguntaActivity extends AppCompatActivity {
     private EditText enunciado, correcto, falso1, falso2, falso3;
     private Spinner spinner;
 
+    final private int CODE_WRITE_EXTERNAL_STORAGE_PERMISSION = 123;
+    private Context myContext;
+    private ConstraintLayout constraintLayoutMainActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         MyLog.d(TAG, "Iniciando onCreate...");
@@ -26,6 +37,11 @@ public class NuevaPreguntaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nueva_pregunta);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Almacenamos el contexto de la actividad para utilizar en las clases internas
+        myContext = this;
+        // Recuperamos el Layout donde mostrar el Snackbar con las notificaciones
+        constraintLayoutMainActivity = findViewById(R.id.layout);
 
         enunciado = (EditText) findViewById(R.id.editTextEnunciado);
         correcto = (EditText) findViewById(R.id.editTextRCorrecta);
@@ -37,7 +53,7 @@ public class NuevaPreguntaActivity extends AppCompatActivity {
         String[] categorias = {"Selecciona una categoría", "Java", "Python", "R", "Go"};
         spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categorias));
 
-        //botón para guardar una nueva pregunta
+        //Botón para guardar una nueva pregunta
         Button botonGuardar = (Button) findViewById(R.id.buttonGuardar);
         botonGuardar.setOnClickListener(new View.OnClickListener() {
 
@@ -47,6 +63,10 @@ public class NuevaPreguntaActivity extends AppCompatActivity {
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(enunciado.getWindowToken(), 0);
 
+                //Permisos de escritura
+                int WriteExternalStoragePermission = ContextCompat.checkSelfPermission(myContext, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                Log.d("NuevaPreguntaActivity", "WRITE_EXTERNAL_STORAGE Permission: " + WriteExternalStoragePermission);
+
                 //Si alguno de los editText está vacío o no se ha seleccionado ninguna categoría, salta el snackbar
                 if(enunciado.getText().toString().isEmpty() || correcto.getText().toString().isEmpty() || falso1.getText().toString().isEmpty() ||
                         falso2.getText().toString().isEmpty() || falso3.getText().toString().isEmpty() ||
@@ -54,12 +74,43 @@ public class NuevaPreguntaActivity extends AppCompatActivity {
                     Snackbar.make(view, "Rellena todos los campos para Guardar.", Snackbar.LENGTH_SHORT)
                             .setAction("Action", null).show();
                 }else{
-                    //GUARDA LA PREGUNTA
+                    if (WriteExternalStoragePermission != PackageManager.PERMISSION_GRANTED){
+                        // Permiso denegado
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                            ActivityCompat.requestPermissions(NuevaPreguntaActivity.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, CODE_WRITE_EXTERNAL_STORAGE_PERMISSION);
+                            // Una vez que se pide aceptar o rechazar el permiso se ejecuta el método "onRequestPermissionsResult" para manejar la respuesta
+                            // Si el usuario marca "No preguntar más" no se volverá a mostrar este diálogo
+                        } else {
+                            Snackbar.make(view, "Permisos de escritura denegados.", Snackbar.LENGTH_SHORT)
+                                    .setAction("Action", null).show();
+                        }
+                    }else {
+                        //GUARDA LA PREGUNTA
+                    }
                 }
             }
         });
 
         MyLog.d(TAG, "Cerrando onCreate...");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case CODE_WRITE_EXTERNAL_STORAGE_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permiso aceptado
+                    Snackbar.make(constraintLayoutMainActivity, "Permisos de escritura aceptados.", Snackbar.LENGTH_SHORT)
+                            .setAction("Action", null).show();
+                } else {
+                    // Permiso rechazado
+                    Snackbar.make(constraintLayoutMainActivity, "Permisos de escritura denegados.", Snackbar.LENGTH_SHORT)
+                            .setAction("Action", null).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
