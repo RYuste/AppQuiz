69040cc35c7067b3b2038c2f4906dcc222d9eccc
Rafael Yuste <teka_ryj@hotmail.com>
2019-01-15 14:04:09 +0100

    Implementación de la funcionalidad de los botones de cámara y galería

--- app/src/main/java/com/appquiz/proyectoappquiz/NuevaEditaPreguntaActivity.java
+++ app/src/main/java/com/appquiz/proyectoappquiz/NuevaEditaPreguntaActivity.java
@@ -2,12 +2,20 @@ package com.appquiz.proyectoappquiz;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -23,9 +31,16 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class NuevaEditaPreguntaActivity extends AppCompatActivity {
 
@@ -34,9 +49,17 @@ public class NuevaEditaPreguntaActivity extends AppCompatActivity {
     private Spinner spinner;
     private ArrayAdapter<String> adapter;
 
+    private final String ruta_fotos = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/misfotos/";
+    private File file = new File(ruta_fotos);
+
     final private int CODE_WRITE_EXTERNAL_STORAGE_PERMISSION = 123;
     private Context myContext;
     private ConstraintLayout constraintLayoutMainActivity;
+
+    private static final int REQUEST_CAPTURE_IMAGE = 200;
+    private static final int REQUEST_SELECT_IMAGE = 201;
+    final String pathFotos = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/demoAndroidImages/";
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -261,7 +284,33 @@ public class NuevaEditaPreguntaActivity extends AppCompatActivity {
                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(enunciado.getWindowToken(), 0);
 
+                    try {
+                        // Se crea el directorio para las fotografías
+                        File dirFotos = new File(pathFotos);
+                        dirFotos.mkdirs();
 
+                        // Se crea el archivo para almacenar la fotografía
+                        File fileFoto = File.createTempFile(getFileCode(),".jpg", dirFotos);
+
+                        // Se crea el objeto Uri a partir del archivo
+                        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                        StrictMode.setVmPolicy(builder.build());
+                        uri = Uri.fromFile(fileFoto);
+                        Log.d(TAG, uri.getPath().toString());
+
+                        // Se crea la comunicación con la cámara
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        // Se le indica dónde almacenar la fotografía
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                        // Se lanza la cámara y se espera su resultado
+                        startActivityForResult(cameraIntent, REQUEST_CAPTURE_IMAGE);
+
+                    } catch (IOException ex) {
+                        Log.d(TAG, "Error: " + ex);
+
+                        Snackbar.make(v, "ERROR!!", Snackbar.LENGTH_SHORT)
+                                .setAction("Action", null).show();
+                    }
                 }
             }
         });
@@ -276,12 +325,83 @@ public class NuevaEditaPreguntaActivity extends AppCompatActivity {
                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(enunciado.getWindowToken(), 0);
 
-
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Selecciona una imágen"),
+                            REQUEST_SELECT_IMAGE);
                 }
             }
         });
 
         MyLog.d(TAG, "Cerrando onCreate...");
+    }
+
+    /**
+     * Método encargado de qué hacer cuando se cierra la cámara o la galería
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case (REQUEST_CAPTURE_IMAGE):
+                if(resultCode == Activity.RESULT_OK){
+                    // Se carga la imagen desde un objeto URI al imageView
+                    ImageView imageView = findViewById(R.id.imageViewFoto);
+                    imageView.setImageURI(uri);
+
+                    // Se le envía un broadcast a la Galería para que se actualice
+                    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                    mediaScanIntent.setData(uri);
+                    sendBroadcast(mediaScanIntent);
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    // Se borra el archivo temporal
+                    File file = new File(uri.getPath());
+                    file.delete();
+                }
+                break;
+
+            case (REQUEST_SELECT_IMAGE):
+                if (resultCode == Activity.RESULT_OK) {
+                    // Se carga la imagen desde un objeto Bitmap
+                    Uri selectedImage = data.getData();
+                    String selectedPath = selectedImage.getPath();
+
+                    if (selectedPath != null) {
+                        // Se leen los bytes de la imagen
+                        InputStream imageStream = null;
+                        try {
+                            imageStream = getContentResolver().openInputStream(selectedImage);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Se transformam los bytes de la imagen a un Bitmap
+                        Bitmap bmp = BitmapFactory.decodeStream(imageStream);
+
+                        // Se carga el Bitmap en el ImageView
+                        ImageView imageView = findViewById(R.id.imageViewFoto);
+                        imageView.setImageBitmap(bmp);
+                    }
+                }
+                break;
+        }
+    }
+
+    /**
+     * Crea un código a partir de la fecha y la hora
+     *
+     * @return date
+     */
+    private String getFileCode(){
+        // Se crea un código a partir de la fecha y hora
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss", java.util.Locale.getDefault());
+        String date = dateFormat.format(new Date());
+        // Se devuelve el código
+        return "pic_" + date;
     }
 
     /**
